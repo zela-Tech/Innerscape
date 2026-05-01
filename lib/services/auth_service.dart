106176b822afe36ba9aa0f5fd16202a930ddd787
Adom Z <zelalemadom@gmail.com>
2026-05-01 11:13:21 -0400

    feat(auth): add Firestore integration to store user profile during registration

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,13 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  Future<UserCredential> register({required String email,required String password,}) async {
-    return await _auth.createUserWithEmailAndPassword(
+  Future<UserCredential> register({required String email,required String password,required String name,required String username,}) async {
+    final credential = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+
+    final user = credential.user;
+
+    if (user != null) {
+      await _db.collection('users').doc(user.uid).set({
+        'email': email,
+        'name': name,
+        'username': username,
+        'createdAt':Timestamp.now(),
+      });
+    }
+    return credential;
   }
 
   Future<UserCredential> signIn({required String email,required String password,}) async {
