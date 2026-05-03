4f6090c7c3f35e5cb208a9a73f562d9db920356d
Adom Z <zelalemadom@gmail.com>
2026-05-03 16:16:07 -0400

    feat(journal-main-ui): add base journal screen ui

--- lib/screens/journal_screen.dart
+++ lib/screens/journal_screen.dart
@@ -1,16 +1,196 @@
 import 'package:flutter/material.dart';
+import './add_Journal_screen.dart';
 
 class JournalScreen extends StatelessWidget {
   const JournalScreen({super.key});
 
+  final List<String> journalCovers = const [
+    "assets/images/green_cover.png",
+    "assets/images/red_cover.png",
+    "assets/images/teal_cover.png",
+    "assets/images/orange_cover.png",
+    "assets/images/pink_cover.png",
+    "assets/images/blue_cover.png",
+    "assets/images/purple_cover.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Journal Screen",
-          style: TextStyle(fontSize: 24),
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6F7F9),
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              const SizedBox(height: 10),
+
+              //header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Journal",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.add_circle_outline),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const AddJournalScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              // feature journals/most recent
+              SizedBox(
+                height: 120,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _topCard("Daily Journal", Colors.redAccent),
+                    _topCard("2026 Goals", Colors.teal),
+                    _topCard("Recipes", Colors.brown),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              //TODO: ADD filter chips 
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "All",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: journalCovers.length,
+                  itemBuilder: (context, index) {
+                    return _journalTile(journalCovers[index]);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _topCard(String title, Color color) {
+    return Container(
+      width: 140,
+      margin: const EdgeInsets.only(right: 12),
+      decoration: BoxDecoration(
+        color: color.withValues(alpha: 0.85),
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _journalTile(String imagePath) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        children: [
+
+          //book cover
+          Container(
+            width: 50,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+                topLeft: Radius.circular(0),
+                bottomLeft: Radius.circular(0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.15),
+                  blurRadius: 10,
+                  offset: const Offset(3, 5),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          const SizedBox(width: 12),
+
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Journal Title",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  "First few texts...",
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+
+          const Icon(Icons.chevron_right),
+        ],
       ),
     );
   }
