3aa03726e753f4477181f5b0ae6ef755c3ea8e81
Adom Z <zelalemadom@gmail.com>
2026-05-03 00:11:44 -0400

    feat(nav):updatebottom navigation with glass-style and blur effect

--- lib/core/navigation/main_nav.dart
+++ lib/core/navigation/main_nav.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../screens/home_screen.dart';
 import '../../screens/journal_screen.dart';
 import '../../screens/insights_screen.dart';
+import 'dart:ui';
 
 class MainNav extends StatefulWidget {
   const MainNav({super.key});
@@ -23,25 +24,47 @@ class _MainNavState extends State<MainNav> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: screens[currentIndex],
-      bottomNavigationBar: Container(
-        height: 70,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-          boxShadow: [
-            BoxShadow(
-              color: const Color.fromRGBO(121, 0, 0, 0.05),
-              blurRadius: 10,
-            )
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _navItem(Icons.home, 0),
-            _navItem(Icons.menu_book, 1),
-            _navItem(Icons.bar_chart, 2),
-          ],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white.withValues(alpha: 0.2),
+                    Colors.white.withValues(alpha: 0.05),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 218, 218, 218).withValues(alpha: 0.25),
+                  width: 1,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.1),
+                    blurRadius: 25,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _navItem(Icons.home, 0),
+                  _navItem(Icons.menu_book, 1),
+                  _navItem(Icons.bar_chart, 2),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
