496afa9cb7bb95c5652eff8c7fc2c6b9e1fa563d
Adom Z <zelalemadom@gmail.com>
2026-05-03 17:56:16 -0400

    feat: updat journal model with boolean to seperate JournalScreen and DailyJournalScreen

--- lib/model/journal_model.dart
+++ lib/model/journal_model.dart
@@ -1,8 +1,9 @@
 class Journal {
   final String title;
   final String cover;
+  final bool isDaily;
   final DateTime updatedAt;
 
 
-  Journal({required this.title, required this.cover, required this.updatedAt,});
+  Journal({required this.title, required this.cover, required this.updatedAt,this.isDaily = false,});
 }
