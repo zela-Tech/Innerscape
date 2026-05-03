5beff0679172511e44f429b6c5809f7b3b976548
Adom Z <zelalemadom@gmail.com>
2026-05-03 19:41:23 -0400

    feat(journal): add Journal data model for Firestore integration

--- lib/model/journal_model.dart
+++ lib/model/journal_model.dart
@@ -1,9 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Journal {
+  final String id;
   final String title;
   final String cover;
   final bool isDaily;
   final DateTime updatedAt;
 
+  Journal({required this.id,required this.title,required this.cover,required this.updatedAt,this.isDaily = false,});
 
-  Journal({required this.title, required this.cover, required this.updatedAt,this.isDaily = false,});
+  factory Journal.fromFirestore(String id, Map<String, dynamic> data) {
+    return Journal(
+      id: id,
+      title: data['title'] ?? '',
+      cover: data['cover'] ?? '',
+      isDaily: data['type'] == 'daily',
+      updatedAt: (data['updatedAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
+    );
+  }
 }
