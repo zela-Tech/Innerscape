597b4f3794a6259cfd2b12b9c3b876a2c4545c99
Adom Z <zelalemadom@gmail.com>
2026-05-03 15:25:15 -0400

    feat: add getJournalEntries method to fetch ordered journal pages

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -87,4 +87,22 @@ class JournalService {
       'updatedAt': FieldValue.serverTimestamp(),
     });
   }
+
+  Future<List<Map<String, dynamic>>> getJournalEntries(String journalId) async {
+    final user = _auth.currentUser;
+    if (user == null) throw Exception("User not logged in");
+
+    final uid = user.uid;
+
+    final snapshot = await _db
+        .collection('users')
+        .doc(uid)
+        .collection('journals')
+        .doc(journalId)
+        .collection('entries')
+        .orderBy('pageNumber')
+        .get();
+
+    return snapshot.docs.map((doc) => doc.data()).toList();
+  }
 }
