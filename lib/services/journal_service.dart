eafae703edd5489e21a9234881c3edce1100282c
Adom Z <zelalemadom@gmail.com>
2026-05-03 20:51:03 -0400

    fix: remove and apply one journal per user logic at sign up

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -5,56 +5,13 @@ class JournalService {
   final _db = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
 
-  Future<String> createOrGetDailyJournal({required String moodLabel,required String moodImage,}) async {
+  Future<void> savePage({required String journalId,required int pageNumber,required String content,}) async {
     final user = _auth.currentUser;
     if (user == null) throw Exception("User not logged in");
 
     final uid = user.uid;
-    final now = DateTime.now();
 
-    final dateKey = "${now.year}-${now.month}-${now.day}";
-
-    final query = await _db
-      .collection('users')
-      .doc(uid)
-      .collection('journals')
-      .where('dateKey', isEqualTo: dateKey)
-      .where('type', isEqualTo: 'daily')
-      .limit(1)
-      .get();
-
-    if (query.docs.isNotEmpty) {
-      return query.docs.first.id;
-    }
-
-    final docRef = await _db
-        .collection('users')
-        .doc(uid)
-        .collection('journals')
-        .add({
-      'title': "Daily Journal",
-      'type': "daily",
-      'moodLabel': moodLabel,
-      'moodImage': moodImage,
-      'dateKey': dateKey,
-      'createdAt': FieldValue.serverTimestamp(),
-    });
-
-    //create first entry page
-    await docRef.collection('entries').add({
-      'content': "",
-      'pageNumber': 1,
-      'createdAt': FieldValue.serverTimestamp(),
-    });
-
-    return docRef.id;
-  }
-
-  Future<void> addEntry({required String journalId,required int pageNumber,required String content,}) async {
-    final user = _auth.currentUser;
-    if (user == null) throw Exception("User not logged in");
-
-    final uid = user.uid;
+    final pageId = "page_$pageNumber";
 
     await _db
         .collection('users')
@@ -62,25 +19,7 @@ class JournalService {
         .collection('journals')
         .doc(journalId)
         .collection('entries')
-        .add({
-      'content': content,
-      'pageNumber': pageNumber,
-      'createdAt': FieldValue.serverTimestamp(),
-    });
-  }
-  Future<void> saveEntry({required String journalId,required int pageNumber,required String content,}) async {
-    final user = _auth.currentUser;
-    if (user == null) throw Exception("User not logged in");
-
-    final uid = user.uid;
-
-    await _db
-        .collection('users')
-        .doc(uid)
-        .collection('journals')
-        .doc(journalId)
-        .collection('entries')
-        .doc("page_$pageNumber")
+        .doc(pageId)
         .set({
       'content': content,
       'pageNumber': pageNumber,
@@ -88,7 +27,7 @@ class JournalService {
     });
   }
 
-  Future<List<Map<String, dynamic>>> getJournalEntries(String journalId) async {
+  Future<List<Map<String, dynamic>>> getPages(String journalId) async {
     final user = _auth.currentUser;
     if (user == null) throw Exception("User not logged in");
 
